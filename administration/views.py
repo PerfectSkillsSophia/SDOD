032b4143eaa2b2ddc1f0229b0002a2114fac4fff
PerfectSkillsSophia <tech@perfectskills.in>
2023-07-04 23:15:37 +0530

    FER UPDATE

--- administration/views.py
+++ administration/views.py
@@ -11,8 +11,56 @@ from sophia import settings
 from .result import *
 from thefuzz import fuzz
 from statistics import mean
-from .faceanalysis import analyze_emotions,analyze_video_emotions
 
+from fer import FER
+import cv2
+
+def analyze_video_emotions(video_path):
+    vid = cv2.VideoCapture(video_path)
+    fps = vid.get(cv2.CAP_PROP_FPS)
+    fps = int(fps)
+    emotion_detector = FER()
+    n = 0
+    i = 0
+    sad1 = fear1 = happy1 = angry1 = surprise1 = disgust1 = neutral1 = 0
+
+    while True:
+        ret, frame = vid.read()
+        if n % fps == 0:
+            attri = emotion_detector.detect_emotions(frame)
+            print(attri)
+            if len(attri) > 0:
+                sad1 += attri[0]["emotions"]['sad']
+                fear1 += attri[0]["emotions"]['fear']
+                happy1 += attri[0]["emotions"]['happy']
+                angry1 += attri[0]["emotions"]['angry']
+                surprise1 += attri[0]["emotions"]['surprise']
+                disgust1 += attri[0]["emotions"]['disgust']
+                neutral1 += attri[0]["emotions"]['neutral']
+                i += 1
+            else:
+                break
+        n += 1
+        if ret == False:
+            break
+
+    vid.release()
+    cv2.waitKey(1)
+    cv2.destroyAllWindows()
+    for i in range(1, 5):
+        cv2.waitKey(1)
+
+    total = sad1 + fear1 + happy1 + angry1 + surprise1 + disgust1 + neutral1
+    confidence = ((happy1 + angry1 + surprise1 + disgust1) / total) * 100
+    nervousness = ((sad1 + fear1) / total) * 100
+    neutral1 = (neutral1 / total) * 100
+
+    if confidence > nervousness:
+        confidence += neutral1
+    else:
+        nervousness += neutral1
+
+    return confidence, nervousness
 
 @staff_member_required
 @login_required(login_url='login')
