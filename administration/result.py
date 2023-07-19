c407ee1b8332c1147b0233402200daa07ad65256
PerfectSkillsSophia <tech@perfectskills.in>
2023-07-19 18:54:48 +0530

    Zero Division error handele

--- administration/result.py
+++ administration/result.py
@@ -16,7 +16,7 @@ nltk.data.path.append('nltk_data')
 
 # nltk.download('wordnet')
 
-def FindAcc(S1, S2):
+""" def FindAcc(S1, S2):
     X = S1.lower()
     Y = S2.lower()
 
@@ -75,7 +75,82 @@ def FindAcc(S1, S2):
     else:
         AccPer = max(AccPer, cosine)
     return AccPer
+ """
 
+import re
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+
+def FindAcc(S1, S2):
+    X = S1.lower()
+    Y = S2.lower()
+
+    S1 = re.split(r'[ ,.!;"()]', X)
+    S2 = re.split(r'[ ,.!;"()]', Y)
+
+    S1.sort()
+    S2.sort()
+
+    Positive = 0
+    Negative = 0
+
+    if len(S1) == 1:
+        if S1[0] in S2:
+            AccPer = 100
+        else:
+            AccPer = 0
+        return AccPer
+
+    if len(S2) == 1:
+        S2.append(".")
+
+    for i in S1:
+        if i == "":
+            continue
+
+        if i in S2:
+            Positive += 1
+        else:
+            Negative += 1
+
+    Total = Positive + Negative
+
+    AccPer = (Positive * 100) / Total
+
+    if Negative < 5:
+        X_list = word_tokenize(X)
+        Y_list = word_tokenize(Y)
+
+        sw = stopwords.words("english")
+        l1 = []
+        l2 = []
+
+        X_set = {w for w in X_list if not w in sw}
+        Y_set = {w for w in Y_list if not w in sw}
+
+        rvector = X_set.union(Y_set)
+        for w in rvector:
+            if w in X_set:
+                l1.append(1)  # create a vector
+            else:
+                l1.append(0)
+            if w in Y_set:
+                l2.append(1)
+            else:
+                l2.append(0)
+        c = 0
+
+        for i in range(len(rvector)):
+            c += l1[i] * l2[i]
+        cosine = c / float((sum(l1) * sum(l2)) ** 0.5)
+
+        if min(AccPer, (cosine * 100)) < 40:
+            AccPer = min(AccPer, cosine)
+        else:
+            AccPer = max(AccPer, cosine)
+
+    return AccPer
 
 
 
