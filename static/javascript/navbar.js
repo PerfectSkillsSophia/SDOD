594c62ed9a6286cc32af2a9ea52a4b9e8270d343
PerfectSkillsSophia <tech@perfectskills.in>
2023-08-11 23:13:05 +0530

    old code

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -1,5 +1,20 @@
-var backButton = document.getElementById('backButton');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var backButton = document.getElementById('backButton');
+  
+//     if (backButton) {
+//       backButton.addEventListener('click', function() {
+//         window.history.back();
+//       });
+//     }
+//   });
+  
+document.addEventListener('DOMContentLoaded', function() {
+  var backButton = document.getElementById('backButton');
 
-backButton.addEventListener('click', function() {
-    window.history.back();
+  if (backButton) {
+      backButton.addEventListener('click', function() {
+          window.history.back();
+          location.reload(); // Reload the page
+      });
+  }
 });
